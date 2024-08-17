2dedc523762daccad6ccaf28a0a1512d23460877
Ronak Sankhala <RonakSankhala@outlook.com>
2024-08-17 22:11:45 +0530

    Complete Design Controllers

---
+++ Models/StudentMetaData.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementApp.MVC.Data;
+
+public class StudentMetaData
+{
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = null!;
+    
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = null!;
+    
+    [Required]
+    [Display(Name = "Date Of Birth")]
+    public DateTime? DateOfBirth { get; set; }
+}
+
+[ModelMetadataType(typeof(StudentMetaData))]
+public partial class Student
+{
+    
+}
